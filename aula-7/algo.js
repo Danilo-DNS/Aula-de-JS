2bbdabe6ba44168b1d631adc82cbe02a0edb2890
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2024-07-11 09:02:28 -0300

    aula js

---
+++ aula-7/algo.js
@@ -0,0 +1,10 @@
+/*function on() {
+    document.querySelector("#lamp").src ="img/acessa.png"
+}
+function off() {
+    document.querySelector("#lamp").src ="img/desligada.png"
+}*/
+
+function trocar(arquivo) {
+    document.querySelector('#personagem').setAttribute('src','img/'+arquivo)
+}
