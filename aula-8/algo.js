57a7fa7cf64615509b6581609e471270e47ebe56
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2024-07-12 09:52:12 -0300

    aula js

--- aula-8/algo.js
+++ aula-8/algo.js
@@ -1,25 +1,25 @@
 function verificar() {
 
 
-        let select = document.querySelector('select').value
+    let select = document.querySelector('select').value
 
-        if (select == "Desabilitado") {
-            document.querySelector("input").disabled = true
+    if (select == "Desabilitado") {
+        document.querySelector("input").disabled = true
 
-            let pElemt = document.querySelector("#messagem")
+        let pElemt = document.querySelector("#messagem")
 
-            pElemt.innerText = "Desabilitado"
-            textos.style.backgroundColor = "blue"
-            textos.style.color = "White"
-        } else {
-          
-            document.querySelector("input").disabled = false
-            let pElemt = document.querySelector("#messagem")
+        pElemt.innerText = "Desabilitado"
+        textos.style.backgroundColor = "blue"
+        textos.style.color = "White"
+    } else {
+      
+        document.querySelector("input").disabled = false
+        let pElemt = document.querySelector("#messagem")
 
-            pElemt.innerText = ""
-            textos.style.backgroundColor = "black"
-            textos.style.color = "transparent"
+        pElemt.innerText = ""
+        textos.style.backgroundColor = "black"
+        textos.style.color = "transparent"
+
+    }
     
-        }
-        
 }
