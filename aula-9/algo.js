12e43f48269ea4b25c02e211bc44406c4d96a815
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2024-07-15 09:10:36 -0300

    aula js

---
+++ aula-9/algo.js
@@ -0,0 +1,12 @@
+let h1 = document.getElementById("valor");
+
+let mais = () =>{
+    
+    h1.innerHTML = Number(h1.innerHTML)+1
+    
+}
+
+let menos = () =>{
+    h1.innerHTML = Number(h1.innerHTML)-1
+    
+}
