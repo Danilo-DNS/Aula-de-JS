de58f142e245107f11e6c7637177e3533ca0d7e4
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2024-07-17 09:38:21 -0300

    aula js

--- aula-10/imc/code.js
+++ aula-10/imc/code.js
@@ -1,18 +1,26 @@
-function trocar(arquivo) {
-    document.querySelector('#genero').setAttribute('src','img/'+arquivo)
+
+
+function troca(arquivo){
+    document.querySelector(".genero_icon").setAttribute('src', './img/'+arquivo)
 }
 
-
-let altura = parseFloat(document.getElementById('altura')).value
-let peso = parseFloat(document.getElementById('peso')).value
-let imc = peso / (altura * altura )
-
-
-console.log(imc)
-
-
-
 function calculo() {
-    alert(imc)
-    
+   let peso = Number(document.querySelector("#peso").value)
+   let altura = Number(document.querySelector("#altura").value)
+
+
+   let imc = (peso / (altura * altura)).toFixed(1)
+
+    if ( imc < 19.5 ){
+    alert("voce está abaixo do peso");
+    }
+    if (imc >= 19.5 && imc < 26.4){
+    alert("Você está com o peso ideal");
+    }
+    if (imc >= 26.4  && imc < 31) {
+    alert(" Você está acima do peso");
+    }
+    if (imc >= 31 ) {
+    alert("Você está obeso");
+    }
 }
